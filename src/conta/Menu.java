236f6641c51971522d7439adff519771495b1856
rbcribeiro <rbcribeiro@gmail.com>
2024-06-16 17:53:49 -0300

    feat: implementando classes das contas Corrente e poupança

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,25 +1,32 @@
 package conta;
 
 import java.util.Scanner;
-import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
+	
+	public static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
-		// Teste da Classe Conta
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
-
-		Scanner leia = new Scanner(System.in);
 
 		int opcao;
-
+		
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
 		while (true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
@@ -47,7 +54,7 @@ public class Menu {
 
 			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
-                  		sobre();
+           		sobre();
 				leia.close();
 				System.exit(0);
 			}
